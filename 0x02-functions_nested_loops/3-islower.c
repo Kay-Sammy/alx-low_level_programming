981da36fa78d97105e54dd159ffb37d16f651cb3
Kay-Sammy <klausapea17@gmail.com>
2022-03-15 16:15:04 -0700

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * Description: program that checks lowercase character
+ * Return: Prints the numbers
+ */
+int main(void)
+{
+	int _islower(int c);
+	{
+	return	(c >= 'a' && c <= 'z');
+	}
+return (0);
+}
