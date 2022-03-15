6c253d010203c9ce6d86bf701dd8da643722d95b
Kay-Sammy <klausapea17@gmail.com>
2022-03-15 16:42:34 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * Description: program that returns 1 if c is a letter, lowercase or uppercase
+ * Return: prints the numbers
+ */
+int main(void)
+{
+	int _isalpha(int c)
+	{
+	return	((c >= 'a' && c<= 'z') || (c >= 'A' && c<= 'Z'));
+	}
+return	(0);
+}
